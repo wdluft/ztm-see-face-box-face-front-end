612204fa1dc75394dfe9028cdc75368a357a0b6e
Will Luft <william.d.luft@gmail.com>
2020-12-31 08:29:15 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
+import ImageLinkForm from './components/image-link-form/ImageLinkForm';
+import Logo from './components/logo/Logo';
+import Navigation from './components/Navigation/Navigation';
+import Rank from './components/rank/Rank';
+import FaceRecognition from './components/face-recognition/FaceRecognition';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+ apiKey: '865c4f2fbc0d495cb26a0d4fcd11bc68'
+});
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+  }
+}
 
 function App() {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [box, setBox] = useState({});
+
+  const onInputChange = e => {
+    setInput(() => e.target.value);
+  }
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  const displayFaceBox = (box) => {
+    setBox(() => box);
+    console.log(box);
+  }
+
+  const onButtonSubmit = () => {
+    setImageUrl(() => input);
+
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
+      .then(res => displayFaceBox(calculateFaceLocation(res)))
+      .catch(err => console.log(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles className='particles' params={particleOptions} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}/>
+      <FaceRecognition imageUrl={imageUrl} box={box}/>
     </div>
   );
 }
---
+++ src/components/rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="black f3">
+        {`Andrei, your current rank is...`}
+      </div>
+      <div className="black f1">
+        {`#5`}
+      </div>
+    </div>
+  )
+}
+
+export default Rank
