dac293933f4d5037f18314ced55a5d358f0078fc
Will Luft <william.d.luft@gmail.com>
2021-01-14 17:15:39 -0500

    Updated face recognition to identify and box multiple faces in a photo

--- src/components/face-recognition/FaceRecognition.js
+++ src/components/face-recognition/FaceRecognition.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl, box }) => (
+const FaceRecognition = ({ imageUrl, boxes }) => (
   <div className="center ma">
-    <div className="absolute mt2">
+    <div className="relative mt2">
       <img id="inputImage" src={imageUrl} alt="" width="500px" height="auto" />
-      <div
-        className="bounding-box"
-        style={{
-          top: box.topRow,
-          right: box.rightCol,
-          bottom: box.bottomRow,
-          left: box.leftCol,
-        }}
-      />
+      {boxes.map((box, i) => (
+        <div
+          key={`box-${i + 1}`}
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        />
+      ))}
     </div>
   </div>
 );
